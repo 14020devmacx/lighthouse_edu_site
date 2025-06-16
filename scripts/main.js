db33e6dd356091da4cf5863f1e0dbf6c0a6c1f75
DEVmacX <shaillmacx@gmail.com>
2025-06-17 01:47:11 +0600

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+/* Main JavaScript file */
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Website loaded successfully!');
+    // Add any global JavaScript functionality here
+}); 
